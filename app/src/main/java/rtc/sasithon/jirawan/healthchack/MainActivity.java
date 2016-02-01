8e45774f296cd98b9722e640a1f113e930fb9fc6
toey <jirawamtoeyty@gmail.com>
2016-02-01 11:38:27 +0700

    ออกแบบ ui main

--- app/src/main/java/rtc/sasithon/jirawan/healthchack/MainActivity.java
+++ app/src/main/java/rtc/sasithon/jirawan/healthchack/MainActivity.java
@@ -2,12 +2,29 @@ package rtc.sasithon.jirawan.healthchack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Explicit ประกาศตัวแปร
+    private ImageView headImageView,BodyImageView,ButtonImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //set Controller to ImageView
+        headImageView.setOnClickListener(this);
+        BodyImageView.setOnClickListener(this);
+        ButtonImageView.setOnClickListener(this);
+
+
+    }// Main Method นี่คือ เมธอต
+
+    @Override
+    public void onClick(View v) {
+
+    }//onClick
+
+}// Main Class
